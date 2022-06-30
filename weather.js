b38ae044da96bf8767762d5040f12e56db8f6212
AmulyaTangirala <86712643+AmulyaTangirala@users.noreply.github.com>
2022-06-30 11:09:01 +0530

    Delete weather.js

--- weather.js
+++
@@ -1,8 +0,0 @@
-async function weather(){
-    let cityname=document.getElementById("city");
-    let url="https://api.openweathermap.org/data/2.5/weather?q="+cityname.value+"&appid=08b13c9102921d534ef352561eda33d6&unit:metrics;"
-    const response=await fetch(url);
-    const data=await response.json();
-    let{main:{temp,humidity,pressure}}=data;
-    console.log("temp:"+temp+"humidity:"+humidity+"pressure:"+pressure);
-}   
