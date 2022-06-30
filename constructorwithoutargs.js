c6b7b0861d7dfd40eead73bc8c01fcdeedeb87f3
AmulyaTangirala <86712643+AmulyaTangirala@users.noreply.github.com>
2022-06-30 11:11:02 +0530

    prog2

---
+++ constructorwithoutargs.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(){
+        this.name="amulya";
+    }
+    display1(){
+        console.log("student name is : "+this.name);
+    }
+}
+let s = new Student();
+s.display1();
